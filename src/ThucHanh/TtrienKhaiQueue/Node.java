ccec567ef971f20909133b15312c0c841ceddeb9
CoraSan1 <nguyendinh070199@gmail.com>
2022-10-03 22:15:36 +0700

    ThucHanh

---
+++ src/ThucHanh/TtrienKhaiQueue/Node.java
@@ -0,0 +1,11 @@
+package ThucHanh.TtrienKhaiQueue;
+
+public class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+}
